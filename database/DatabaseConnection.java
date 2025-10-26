fa4cdf539aefdc3022125dad60e48d33ece661df
achmadrafiqsyadid <bandspacemo@gmail.com>
2025-10-26 17:53:34 +0800

    Menyiapkan package MVC dan Pengaturan Database

---
+++ database/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package Latihan3.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+private static final String URL = "jdbc:sqlite:contacts.db";
+public static Connection getConnection() throws SQLException {
+return DriverManager.getConnection(URL);
+}
+}
